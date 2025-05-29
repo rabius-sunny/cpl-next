944ee98189572e158b2a2ac5e85abf475bf98389
romancmm <romancmm@gmail.com>
2025-05-29 16:12:18 +0600

    Update: product card

--- components/homepage/products.tsx
+++ components/homepage/products.tsx
@@ -1,7 +1,6 @@
 import { getProducts } from "@/actions/data/product";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
+import ProductCard from "../common/ProductCard";
 
 type TPops = {
     data?: StatsSection
@@ -15,16 +14,9 @@ export default async function ProductsSection({ data }: TPops) {
     return (
         <section id="about_us" className={cn("py-20 bg-white")}>
             <div className="mx-auto px-4 max-w-7xl container">
-                <div className="gap-8 grid grid-cols-3">
+                <div className="gap-8 grid grid-cols-none lg:grid-cols-3">
                     {products?.data?.map((item: Product, index: number) => (
-                        <div key={index}>
-                            <Image src={item.thumbnail?.file || ''} height={400} width={400} alt="" />
-                            <div id="description" className="space-y-4 bg-secondary p-5">
-                                <h4 className="font-semibold text-lg">{item?.name}</h4>
-                                <p className="">{item?.description}</p>
-                                <Link href={'/#'}>See more</Link>
-                            </div>
-                        </div>
+                        <ProductCard item={item} key={index} />
                     ))}
                 </div>
             </div>
