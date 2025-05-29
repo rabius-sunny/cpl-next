a011346a3c6021c5379d020578dd4e78f8b5188f
romancmm <romancmm@gmail.com>
2025-05-29 16:31:57 +0600

    Update product section

--- components/homepage/products.tsx
+++ components/homepage/products.tsx
@@ -10,10 +10,15 @@ type TPops = {
 export default async function ProductsSection({ data }: TPops) {
     const products = await getProducts()
 
-    console.log('result :>> ', products);
     return (
         <section id="about_us" className={cn("py-20 bg-white")}>
-            <div className="mx-auto px-4 max-w-7xl container">
+            <div className="space-y-28 mx-auto px-4 max-w-7xl container">
+                <div className="flex lg:flex-row flex-col justify-between gap-6 w-full">
+                    <h2 className="font-bold text-secondary text-3xl lg:text-5xl">Our Products</h2>
+                    <div className="max-w-lg text-gray-500">
+                        <p>CPML continuously strives to build a reputation for delivery of quality products and services of all scales and complexity, to the satisfaction of our customers.</p>
+                    </div>
+                </div>
                 <div className="gap-8 grid grid-cols-none lg:grid-cols-3">
                     {products?.data?.map((item: Product, index: number) => (
                         <ProductCard item={item} key={index} />
