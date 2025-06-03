736333df48decd221c5cd3e67f1968300b04b087
rabius-sunny <rabiussunnyc3x@gmail.com>
2025-06-03 18:53:01 +0600

    update: about us page

--- app/(frontend)/about/page.tsx
+++ app/(frontend)/about/page.tsx
@@ -1,4 +1,5 @@
 import { retrieveAboutus } from '@/actions/data/homepage'
+import Image from 'next/image'
 
 export default async function page() {
   const data = await retrieveAboutus()
@@ -6,5 +7,39 @@ export default async function page() {
     return <div>Error loading data</div>
   }
   console.log('data', data.data)
-  return <div>page</div>
+  return (
+    <div className='grid gap-8 md:gap-12 lg:gap-16'>
+      <div>
+        <Image
+          src={data.data.backgroundImage?.file || '/placeholder.webp'}
+          alt='Background'
+          width={1920}
+          height={1080}
+          className='w-full h-auto object-cover'
+        />
+      </div>
+      <div className='box'>
+        {data.data?.sections?.map((section, idx) => (
+          <div key={idx} className='grid grid-cols-1 lg:grid-cols-2 gap-12'>
+            <div>
+              <Image
+                src={section.image?.file || '/placeholder.webp'}
+                alt={'Section Image'}
+                width={1920}
+                height={900}
+                className='size-full object-cover'
+              />
+            </div>
+            <div>
+              <h2 className='text-3xl text-primary  font-bold'>{section.title}</h2>
+              <p className='mt-4'>{section.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+    </div>
+  )
 }
