6b6a2f771ff81a3a33e3a9e30968fc791c0e0110
romancmm <romancmm@gmail.com>
2025-06-15 13:19:19 +0600

    Update: mobile menu drawer

---
+++ components/layout/MobileMenu.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
+import { MobileMenuItem } from "./MultiLevelMenu";
+
+type TProps = {
+    items?: any
+}
+
+export default function MobileMenu({ items }: TProps) {
+    const [open, setOpen] = useState<boolean>(false)
+    const pathname = usePathname()
+
+    useEffect(() => {
+        setOpen(false)
+    }, [pathname])
+
+    return (
+        <Sheet open={open} onOpenChange={setOpen}>
+            <SheetTrigger onClick={() => setOpen(true)}>
+                <span className="group lg:hidden flex flex-col justify-center items-center gap-1.5 p-2 border border-transparent rounded-md focus:outline-none focus:ring-0 focus:ring-primary focus:ring-offset-0 font-medium text-white text-sm cursor-pointer">
+                    <span className="bg-gray-800 w-6 h-[3px]" />
+                    <span className="bg-gray-800 -mr-5 group-hover:-mr-0 w-6 h-[3px] transition-all duration-300 ease-in-out transform" />
+                    <span className="bg-gray-800 w-6 h-[3px]" />
+                </span>
+            </SheetTrigger>
+            <SheetContent className="bg-white !border-none w-[85%] max-w-sm">
+                <SheetHeader>
+                    <SheetTitle className=''>Menu</SheetTitle>
+                    <SheetClose />
+                </SheetHeader>
+
+
+                <SheetDescription asChild>
+                    <div className="relative pb-20 max-w-full overflow-y-auto">
+                        {items?.map((item: NavItem, index: number) => (
+                            <MobileMenuItem key={index} item={item} />
+                        ))}
+                    </div>
+                </SheetDescription>
+            </SheetContent>
+        </Sheet>
+    )
+}
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,15 +1,17 @@
 import { retrieveHomepage } from '@/actions/data/homepage'
-import { SheetHeader } from '@/components/ui/sheet'
 import Image from "next/image"
 import Link from "next/link"
 import AnimatedButton from '../common/AnimatedButton'
-import { DialogTitle } from '../ui/dialog'
-import { Sheet, SheetClose, SheetContent, SheetDescription, SheetTrigger } from "../ui/sheet"
-import ResponsiveMenu, { MobileMenuItem } from './MultiLevelMenu'
+// import MobileMenu from './MobileMenu'
+import dynamic from 'next/dynamic'
+import ResponsiveMenu from './MultiLevelMenu'
+
+const MobileMenu = dynamic(() => import('./MobileMenu'), {})
 
 export default async function Header() {
   const data = await retrieveHomepage()
   const siteData = data?.data?.nav
+
 
   return (
     <header className="top-0 z-50 sticky bg-white px-4 py-2 lg:py-4 w-full">
@@ -28,32 +30,11 @@ export default async function Header() {
               </AnimatedButton>}
           </div>
 
-          <div className="lg:hidden block">
-            <Sheet>
-              <SheetTrigger>
-                <span className="group lg:hidden flex flex-col justify-center items-center gap-1.5 p-2 border border-transparent rounded-md focus:outline-none focus:ring-0 focus:ring-primary focus:ring-offset-0 font-medium text-white text-sm cursor-pointer">
-                  <span className="bg-gray-800 w-6 h-[3px]" />
-                  <span className="bg-gray-800 -mr-5 group-hover:-mr-0 w-6 h-[3px] transition-all duration-300 ease-in-out transform" />
-                  <span className="bg-gray-800 w-6 h-[3px]" />
-                </span>
-              </SheetTrigger>
-              <SheetContent className="bg-white !border-none w-[85%] max-w-sm">
-                <SheetHeader>
-                  <DialogTitle className=''>Menu</DialogTitle>
-                  <SheetClose />
-                </SheetHeader>
-
-
-                <SheetDescription asChild>
-                  <div className="relative pb-20 max-w-full overflow-y-auto">
-                    {siteData?.items?.map((item, index) => (
-                      <MobileMenuItem key={index} item={item} />
-                    ))}
-                  </div>
-                </SheetDescription>
-              </SheetContent>
-            </Sheet>
-          </div>
+          {siteData &&
+            <div className="lg:hidden block">
+              <MobileMenu items={siteData?.items} />
+            </div>
+          }
         </nav >
       </div >
     </header >
