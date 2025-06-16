64b3c814f1f20a4acecc01693cb1bfaefdacfd1f
romancmm <romancmm@gmail.com>
2025-06-16 12:53:58 +0600

    Update:  Statistics responsive issues

--- components/homepage/statistics.tsx
+++ components/homepage/statistics.tsx
@@ -18,11 +18,11 @@ export default function StatisticsSection({ data }: TPops) {
                     <h2 className='font-bold text-3xl lg:text-5xl'>{firstWord} <span className='text-primary'>{rest}</span></h2>
                     {data?.backgroundImage?.file && <Image src={data?.backgroundImage?.file} width={1920} height={1200} alt="" />}
 
-                    <div className="flex lg:flex-row flex-col gap-6 shadow-gray-200 shadow-md rounded-xl w-full">
+                    <div className="flex sm:flex-row flex-col gap-6 shadow-gray-200 shadow-md rounded-xl w-full">
                         {data?.stats?.map((data, index) => (
                             <div className="flex flex-col justify-center items-center gap-4 px-6 py-6 lg:py-10 text-center basis-full lg:basis-1/3" key={index}>
-                                <AnimatedCounter to={data?.count ?? 0} className='font-black text-primary text-7xl lg:text-9xl' />
-                                <h2 className="font-bold text-primary lg:text-xl uppercase">{data?.title}</h2>
+                                <AnimatedCounter to={data?.count ?? 0} className='font-black text-primary sm:text-6xl text-7xl lg:text-9xl' />
+                                <h2 className="font-bold text-primary sm:text-sm lg:text-xl uppercase">{data?.title}</h2>
                             </div>
                         ))}
                     </div>
